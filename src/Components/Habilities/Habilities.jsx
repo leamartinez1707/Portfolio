dd6ca5192e39fcd8d4130865644c5262c0d6a099
Leandro Martínez <leandromart@hotmail.com>
2024-03-02 02:17:49 -0300

    updates

--- src/Components/Habilities/Habilities.jsx
+++ src/Components/Habilities/Habilities.jsx
@@ -2,7 +2,7 @@ const Habilities = () => {
     return (
         <>
             <div>
-                <h3 className="my-2 sm:my-4 text-5xl underline p-2 tec-title cursor-pointer">Tecnologías</h3>
+                <h3 className="my-2 sm:my-4 text-5xl underline p-2 tec-title cursor-pointer uppercase">Tecnologías</h3>
             </div>
             <div className="habilities-icons my-4 justify-center max-w-4xl mx-auto grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 sm:p-6">
                 <div className="mx-4 my-2 size-18"><img className="icon-habilities py-2 hover:cursor-pointer mx-auto" src="https://skillicons.dev/icons?i=html" alt="HTML icon" /><h6><a href="https://developer.mozilla.org/es/docs/Web/HTML" target="blank">HTML</a></h6></div>
@@ -78,43 +78,38 @@ const Habilities = () => {
                     </h6>
                 </div>
             </div>
-            <h3 className="my-2 sm:my-4 text-5xl underline p-2 tec-title cursor-pointer">Habilidades técnicas</h3>
+            <h3 className="my-2 sm:my-4 text-5xl underline p-2 tec-title cursor-pointer uppercase">Habilidades técnicas</h3>
 
-            <div className="max-w-4xl justify-center mx-auto grid grid-cols-1 sm:grid-cols-2 gap-4 p-6">
-
-                <div className="p-2 max-w-md md:max-w-lg w-full mx-auto uppercase">
-                    <div className="flex rounded-lg h-full p-8 flex-col info-div">
-                        <h5 className="text-2xl">HTML</h5>
-                        <h5 className="text-2xl">CSS</h5>
-                        <h5 className="text-2xl">JAVASCRIPT</h5>
-                        <h5 className="text-2xl">REACT</h5>
-                    </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 max-w-4xl justify-center mx-auto">
+                <div className="w-full max-w-1/2 sm:w-full p-6 uppercase mx-auto">
+                    <h6 className="text-lg sm:text-2xl m-2 text-white">HTML</h6>
+                    <h6 className="text-lg sm:text-2xl m-2 text-white">CSS</h6>
+                    <h6 className="text-lg sm:text-2xl m-2 text-white">REACT</h6>
+                    <h6 className="text-lg sm:text-2xl m-2 text-white">JS</h6>
                 </div>
-                <div className="p-2 max-w-md md:max-w-lg w-full mx-auto uppercase">
-                    <div className="flex rounded-lg h-full p-8 flex-col info-div">
-                        <h5 className="text-2xl">Diseño web adaptable</h5>
-                        <h5 className="text-2xl">CSS | Frameworks</h5>
-                        <h5 className="text-2xl">NPM | Gestión de paquetes</h5>
-                        <h5 className="text-2xl">GIT | Version control</h5>
-                    </div>
+                <div className="w-full max-w-1/2 sm:w-full  p-6 uppercase mx-auto">
+                    <h6 className="text-lg sm:text-2xl m-2 text-white">Diseño web adaptable</h6>
+                    <h6 className="text-lg sm:text-2xl m-2 text-white">CSS | Frameworks</h6>
+                    <h6 className="text-lg sm:text-2xl m-2 text-white">NPM | Gestión de paquetes</h6>
+                    <h6 className="text-lg sm:text-2xl m-2 text-white">GIT | Version control</h6>
                 </div>
             </div>
             <div>
-                <h3 className="my-2 sm:my-4 text-5xl underline p-2 tec-title cursor-pointer">Habilidades blandas</h3>
+                <h3 className="my-2 sm:my-4 text-5xl underline p-2 tec-title cursor-pointer uppercase">Habilidades blandas</h3>
             </div>
-            <div className="p-2 max-w-md md:max-w-lg w-full mx-auto uppercase">
-                <div className="flex rounded-lg h-full p-8 flex-col info-div">
-                    <h5 className="text-2xl">Adaptabilidad</h5>
-                    <h5 className="text-2xl">Comunicación</h5>
-                    <h5 className="text-2xl">Creatividad</h5>
-                    <h5 className="text-2xl">Gestión del tiempo</h5>
-                    <h5 className="text-2xl">Iniciativa</h5>
-                    <h5 className="text-2xl">Pensamiento crítico</h5>
-                    <h5 className="text-2xl">Resolución de problemas</h5>
-                    <h5 className="text-2xl">Trabajo en equipo</h5>
-
+            <div className="grid max-w-4xl justify-center mx-auto">
+                <div className="w-full max-w-1/2 sm:w-full p-6 uppercase mx-auto">
+                    <h6 className="text-lg sm:text-2xl m-2 text-white">Adaptabilidad</h6>
+                    <h6 className="text-lg sm:text-2xl m-2 text-white">Comunicación</h6>
+                    <h6 className="text-lg sm:text-2xl m-2 text-white">Creatividad</h6>
+                    <h6 className="text-lg sm:text-2xl m-2 text-white">Gestión del tiempo</h6>
+                    <h6 className="text-lg sm:text-2xl m-2 text-white">Iniciativa</h6>
+                    <h6 className="text-lg sm:text-2xl m-2 text-white">Pensamiento crítico</h6>
+                    <h6 className="text-lg sm:text-2xl m-2 text-white">Resolución de problemas</h6>
+                    <h6 className="text-lg sm:text-2xl m-2 text-white">Trabajo en equipo</h6>
                 </div>
             </div>
+
         </>
 
     )
