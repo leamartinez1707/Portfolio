67b33e318198607c4dc10cd13e510959847f6299
Leandro Martínez <leandromart@hotmail.com>
2024-03-12 01:17:04 -0300

    update: proyects add weather

--- src/Components/Proyects/Proyects.jsx
+++ src/Components/Proyects/Proyects.jsx
@@ -47,13 +47,32 @@ const Proyects = () => {
                         <p className="py-4 text-base">
                             Proyecto final del curso Backend. Creamos una API REST funcional, utilizando vistas con Handlebars.js. Permitiendo a los usuarios poder realizar compras, con pasarela de pago STRIPE. El usuario ADMIN puede realizar CRUD de productos y editar o eliminar usuarios. Documentación de la API con Swagger.
                         </p>
-                        <a href="https://react-proyect2023.vercel.app/" className="btn-ver mx-auto text-lg p-2 h-12 w-48 overflow-hidden rounded-lg  text-white shadow" target="blank">VER
+                        <a href="https://elemshop.up.railway.app/" className="btn-ver mx-auto text-lg p-2 h-12 w-48 overflow-hidden rounded-lg  text-white shadow" target="blank">VER
                         </a>
                     </div>
                 </div>
-
+                <div
+                    className="sm:max-w-md lg:max-w-3xl mx-auto block rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-white">
+                    <div className="relative overflow-hidden bg-cover bg-no-repeat">
+                        <img
+                            className="rounded-t-lg "
+                            src="/617shots_so.webp"
+                            alt="" />
+                    </div>
+                    <div className="p-2 md:p-10 flex flex-col ">
+                        <h4
+                            className="text-xl leading-tight font-bold">
+                            APP Clima
+                        </h4>
+                        <p className="py-4 text-base">
+                            Mini proyecto de una App del clima, donde se puede consultar el tiempo en cualquier ciudad del mundo.
+                        </p>
+                        <a href="https://appclima-lemon.vercel.app/" className="btn-ver mx-auto text-lg p-2 h-12 w-48 overflow-hidden rounded-lg  text-white shadow" target="blank">VER
+                        </a>
+                    </div>
+                </div>
+                <h1 className='text-3xl m-8'>Proyectos profesionales</h1>
             </div>
-            <h1 className='text-3xl m-8'>Proyectos profesionales</h1>
         </>
     )
 }
